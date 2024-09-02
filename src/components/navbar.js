e961b323b4b368c83bd232213ce416cc93a3dcd6
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-02 17:55:47 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,54 +24,38 @@ const Navbar = () => {
           class="ml-auto collapse navbar-collapse text-white"
           id="navbarSupportedContent"
         >
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-uppercase">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-uppercase justify-content-right">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link active" href="#">
                 About Us
               </a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Blogs
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                BLOGS
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                CONTACT
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                LOGIN
+              </a>
+            </li>
+            <li>
+              <button class="btn btn-outline-light" type="submit">
+                Register
+              </button>
             </li>
           </ul>
-
-          <button class="btn btn-outline-light" type="submit">
-            Register
-          </button>
         </div>
       </div>
     </nav>
