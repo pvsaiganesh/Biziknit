059bcad2765ae08b7fd373e20a9ef9fab0db45a5
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-03 11:48:38 +0530

    Update testimonial-item.js

--- src/components/testimonial-item.js
+++ src/components/testimonial-item.js
@@ -5,15 +5,15 @@ import "./testimonials.scss";
 const TestimonialItem = ({ review, name, testiImg, slideOption }) => {
   return (
     <div
-      class="d-flex flex-row justify-content-center p-5 pt-2 pb-2 text-start "
+      class="d-flex flex-row justify-content-center  text-start "
       data-bs-slide={slideOption}
     >
-      <div class="pr-5">
+      <div class="pe-5 ps-5">
         <p class="fs-5 title pb-2">Business Testimonial</p>
         <p class="fs-6 pb-2">{review}</p>
         <p class="fs-6 pb-2">{name}</p>
       </div>
-      <div class="pl-5">
+      <div class="ps-5 pe-5 ">
         <img src={testiImg} alt="img" />
       </div>
     </div>
