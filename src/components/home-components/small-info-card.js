3a0af802f11fbad3fe45417e7de6aa260021ce73
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-04 14:46:37 +0530

    Update small-info-card.js

--- src/components/home-components/small-info-card.js
+++ src/components/home-components/small-info-card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SmallInfoCard = ({ number, title }) => {
   return (
-    <div class="text-middle">
+    <div class="text-center">
       <p class="fs-4 mb-0">{number}</p>
       <p class="fs-6">{title}</p>
     </div>
