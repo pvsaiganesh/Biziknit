d53f2b1eb8df43d8154299fe09a29c79041e3cc1
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-09 11:18:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/home-components/navbar";
 import Home from "./components/home";
 import Register from "./components/register";
 import Login from "./components/login";
+import AboutUs from "./components/aboutus";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -22,6 +23,15 @@ function App() {
           ></Route>
           <Route element={<Register />} path="/register"></Route>
           <Route element={<Login />} path="/login"></Route>
+          <Route
+            element={
+              <>
+                <Navbar />
+                <AboutUs />
+              </>
+            }
+            path="/aboutus"
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/aboutus.js
@@ -0,0 +1,33 @@
+import "./aboutus.scss";
+import React from "react";
+import img from "../assets/vision.jpg";
+const AboutUs = () => {
+  return (
+    <div>
+      <div className="bg-9 d-flex flex-column justify-content-center align-items-center">
+        <span className="heading-text">About Us</span>
+      </div>
+      <div className="container p-5">
+        <div className="row">
+          <div className="col-6 text-start">
+            <p className="fs-4 text-orange-color">About Us</p>
+            <p className="fs-6">
+              At Biziknit, we understand the challenges faced by small business
+              owners in today's competitive landscape. That's why we've created
+              a unique platform that unites entrepreneurs in a specific pin
+              code, enabling you to thrive together. By becoming a member, you
+              aren't just joining a community; you are stepping into a world of
+              possibilities where you’ll access valuable referrals, automated
+              lead generation, and collaborative growth opportunities.
+            </p>
+          </div>
+          <div className="col-6">
+            <img src={img} alt="img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
