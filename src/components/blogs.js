9d77141c00dd8ea278f9fc1bd47e9ebb8c4dd0ba
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-09 14:34:23 +0530

    updated

--- src/components/blogs.js
+++ src/components/blogs.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Blogs from "./home-components/blogs";
+import { Splash } from "../assets/splash";
+import "./blogs.scss";
 
 const BlogsPage = () => {
   return (
-    <div className="vh-100">
-      <div className="h-30 bg-8"></div>
+    <div>
+      <div className="bg-8 d-flex flex-column justify-content-center align-items-center">
+        <div style={{ position: "relative" }} className="svg-props">
+          <Splash />
+        </div>
+        <div style={{ position: "absolute" }} className="heading-text">
+          Blogs
+        </div>
+      </div>
       <div>
         <Blogs />
       </div>
