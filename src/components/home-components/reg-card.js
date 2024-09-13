73e1bde8de8ac19479697a2236e1bf9814c42a01
pvsaiganesh <ipvsaiganesh@gmail.com>
2024-09-13 18:18:17 +0530

    updated design

--- src/components/home-components/reg-card.js
+++ src/components/home-components/reg-card.js
@@ -1,34 +1,50 @@
 import React from "react";
 import "./reg-card.scss";
-// import InputComp from "./input-comp";
 import { Link } from "react-router-dom";
+import modalImg from "../../assets/modal-img.svg";
 
 const RegCard = () => {
   return (
-    <div className="bg-4 p-5 text-white d-flex flex-column justify-content-center align-items-center">
-      <div className="d-flex flex-column flex-md-row justify-content-between w-100 text-center text-md-start">
-        <div>
-          <p className="fs-5 mb-0 fw-bold">
-            Join us and be a part of the potential business group
-          </p>
-          <p className="fs-6">Helping you in finding potential prospects</p>
-        </div>
-        {/* <div className="d-flex flex-row justify-between">
-            <InputComp placeholder={"Name"} />
-            <InputComp placeholder={"Phone number"} />
-            <InputComp placeholder={"Business name"} />
+    <div className="bg-4 ">
+      <div className="p-5 text-white d-flex flex-column justify-content-center align-items-center">
+        <div className="d-flex flex-column flex-md-row justify-content-between w-100 text-center text-md-start">
+          <div>
+            <p className="fs-5 mb-0">
+              Join us and be a part of the potential business group
+            </p>
+            <p className="fs-6">Helping you in finding potential prospects</p>
           </div>
-          <div className="d-flex flex-row justify-between">
-            <InputComp placeholder={"Business type"} />
-            <InputComp placeholder={"Pin code"} />
-            <InputComp placeholder={"Want to Join Us"} dropdown={true} />
-          </div> */}
-        <div>
-          <Link to={"/register"}>
-            <button type="submit" className="btn btn-outline-light">
-              Register Now
-            </button>
-          </Link>
+          <div>
+            <Link to={"/register"}>
+              <button type="submit" className="btn btn-outline-light">
+                Register Now
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <p
+        data-bs-toggle="modal"
+        data-bs-target="#registerModal"
+        className="fs-6 text-white text-decoration-underline text-end pe-5 pb-2 register-link"
+      >
+        Why you should register?
+      </p>
+
+      <div
+        className="modal fade"
+        id="registerModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content rounded-3  bg">
+            <div className="modal-body">
+              <img src={modalImg} alt="img" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
